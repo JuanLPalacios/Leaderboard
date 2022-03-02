0bf7cf295821d13fec583706829fb22844bc226a
JuanLP <juan.luis.palacios.p@gmail.com>
2022-03-02 11:05:13 -0500

    Refactor cost key to GAME_KEY for readability

--- src/modules/funtions.js
+++ src/modules/funtions.js
@@ -1,14 +1,14 @@
-const key = 'dLFTQGSJZwzmh6iLdNsp';
+const GAME_KEY = 'dLFTQGSJZwzmh6iLdNsp';
 const BASE_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
 
 export const add = async (item) => {
   item.score = Number.parseInt(item.score, 10);
   const body = JSON.stringify(item);
-  const { result } = await fetch(`${BASE_URL}games/${key}/scores/`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body }).then((response) => response.json());
+  const { result } = await fetch(`${BASE_URL}games/${GAME_KEY}/scores/`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body }).then((response) => response.json());
   return result;
 };
 
 export const get = async () => {
-  const { result } = await fetch(`${BASE_URL}games/${key}/scores/`).then((response) => response.json());
+  const { result } = await fetch(`${BASE_URL}games/${GAME_KEY}/scores/`).then((response) => response.json());
   return result;
 };
