22f4b7c5596d982d49e046bfe941e20c4d9b28a6
JuanLP <juan.luis.palacios.p@gmail.com>
2022-03-02 02:17:47 -0500

    Add basic functionality

---
+++ src/modules/app.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-underscore-dangle */
+import { add, get, init } from './funtions';
+
+export default class App {
+  constructor(list, form, refresh) {
+    this.list = list;
+    this.form = form;
+    this.refresh = refresh;
+    this._scores = [];
+  }
+
+  async init() {
+    await init();
+    this.form.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      const newScore = {};
+      new FormData(this.form).forEach((value, key) => { newScore[key] = value; });
+      await add(newScore);
+      this.scores = await get();
+    });
+    this.refresh.addEventListener('click', async (e) => {
+      e.preventDefault();
+      this.scores = await get();
+    });
+  }
+
+  get scores() {
+    return this._scores;
+  }
+
+  set scores(val) {
+    this._scores = val;
+    this._scores.sort((a, b) => b.score - a.score);
+    this.update();
+  }
+
+  update() {
+    this.list.innerHTML = this._scores.map((item) => `<li>${item.name}:${item.score}</li>`).join('');
+  }
+}
